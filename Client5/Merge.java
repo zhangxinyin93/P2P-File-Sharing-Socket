9d6a234faee6c5fbee6edad84e1ad1e68bfa646f
Xinyin_gators <zxyatbeijing@gmail.com>
2015-12-19 22:43:15 -0500

    Client5's code
    Same as the Client5

---
+++ Client5/Merge.java
@@ -0,0 +1,47 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+//import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Merge{
+	public static void merge(LinkedList<Integer> chunkList, String filename){
+		FileInputStream fis;
+        BufferedInputStream bis;
+        BufferedOutputStream bos;
+        FileOutputStream fos;
+        try{ 
+        	//String filename="merge.pptx";
+                fos = new FileOutputStream(filename);
+                bos = new BufferedOutputStream(fos);                  
+                for(int i=0;i<chunkList.size();i++)
+                {
+                		String filepath = Integer.toString(i+1);
+                        fis = new FileInputStream(filepath);
+                        bis = new BufferedInputStream(fis);
+                        int b;
+                        while((b=bis.read())!= -1)
+                        {
+                                bos.write(b);
+                        }
+                		System.out.println("write file "+filepath);
+
+                }
+                bos.close();
+        }
+catch(FileNotFoundException ex)
+        {
+                System.out.println("File Not Found");
+        }
+        catch(IOException ex)
+        {
+                System.out.println("Some IOException occurred");
+        }
+	}
+}
